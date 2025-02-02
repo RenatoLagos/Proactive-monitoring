d2bba98c83e53651eea376dd9faeb307cbfcfe0d
RenatoLagos <renato.lagos.a@gmail.com>
2025-02-02 16:04:34 -0300

    Third commit

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,21 @@
-export function sum() {
-    console.log(2+2)
+import express from "express";
+import router from "./router";
+import database from "./config/db";
+
+// Connect to the database
+async function connect() {
+    try {
+        await database.authenticate();
+        database.sync();
+        console.log("Connection to the database has been established successfully.");
+    } catch (error) {
+        console.error("Error connecting to the database: ", error);   
+    }
 }
+
+connect();
+const server = express();
+
+server.use('/api', router);
+
+export default server;
